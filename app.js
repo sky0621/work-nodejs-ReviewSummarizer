58344abac2ddb673c2a7e0d49799c26bdd5558c6
S.S <ss@ss.com>
2016-09-12 04:32:00 +0900

    process port only

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Heroku!\n');
-}).listen(process.env.PORT || 8929, '127.0.0.1');
+}).listen(process.env.PORT);
 
-console.log('Server running at http://127.0.0.1/');
+console.log('Server running');
 
